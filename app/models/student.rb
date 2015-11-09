836eed4e6bc70c68129295f6272201d8f39359b0
Danny Debevec <daniel.debevec@gmail.com>
2015-11-08 19:59:51 -0700

    Added validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,7 @@
 class Student < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, :format => {with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/}
+  validates :cell_phone, format: {with: /\d{3}-\d{3}-\d{4}/,
+      message: 'format xxx-xxx-xxxx'}
 end
